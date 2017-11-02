464ae3411d47645a8d7761bab8706d52367b164f
Michael Reeder <reeder@knoesis.org>
2017-11-02 14:50:17 -0400

    correctly batched usd buying in one stripe request

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,11 @@
 let config = require('config');
 let EthereumTx = require('ethereumjs-tx');
+let pasync = require('pasync');
 
 let client = require('../utils/redis');
 let web3 = require('../utils/web3');
 let wallet = require('../utils/wallet');
+
 
 const { secretKey } = config.stripe;
 const stripe = require('stripe')(secretKey);
@@ -13,72 +15,71 @@ const gwei = 1000000000;
 module.exports = (app) => {
   app.post('/buy-ticket', async(req, res, next) => {
     try {
-      let { token, ticketAddress, walletAddress: userWallet } = req.body;
+      let { token, ticketAddresses, walletAddress: userWallet, fees } = req.body;
       token = JSON.parse(token);
 
-      console.log('userWallet', userWallet);
+      console.log('ticketAddresses', ticketAddresses);
 
       // REDIS: get contract abies
       let contractInfo;
       let reply = await client.getAsync('terrapin-station');
       contractInfo = JSON.parse(JSON.parse(reply).abis);
 
-      // ETHEREUM: set new owner
-      let ticketInstance = new web3.eth.Contract(contractInfo.ticket.abi, ticketAddress);
-
-      console.log('ticket master:', await ticketInstance.methods.master().call());
-      console.log(wallet.address);
-
+      let total = 0 + fees;
+      for (let i = 0; i < ticketAddresses.length; i++) {
+        let ticketAddress = ticketAddresses[i];
+        let ticketInstance = new web3.eth.Contract(contractInfo.ticket.abi, ticketAddress);
+        let price = parseInt(await ticketInstance.methods.usdPrice().call());
+        total += price;
+      }
 
       // STRIPE: charge
       await stripe.charges.create({
-        amount: await ticketInstance.methods.usdPrice().call(),
-        // amount: 2000,
+        amount: total,
         currency: 'usd',
         source: 'tok_visa', // token.card
         description: 'Charge for ethan.robinson@example.com'
       });
 
-      let oldOwner = await ticketInstance.methods.owner().call();
-      console.log('oldOwner:', oldOwner);
+      await pasync.eachSeries(ticketAddresses, async(ticketAddress) => {
+        // ETHEREUM: set new owner
+        let ticketInstance = new web3.eth.Contract(contractInfo.ticket.abi, ticketAddress);
 
-      let nonce = await web3.eth.getTransactionCount(wallet.address);
-      let chainId = await web3.eth.net.getId();
-      let gas = `0x${(4700000).toString(16)}`;
-      let gasPrice = `0x${(gwei * 20).toString(16)}`;
+        let oldOwner = await ticketInstance.methods.owner().call();
+        console.log('oldOwner:', oldOwner);
 
-      // let ticketPrice = parseInt(await ticketInstance.methods.price().call());
+        let nonce = await web3.eth.getTransactionCount(wallet.address);
+        let chainId = await web3.eth.net.getId();
+        let gas = `0x${(4700000).toString(16)}`;
+        let gasPrice = `0x${(gwei * 20).toString(16)}`;
 
-      let encodedAbi = ticketInstance.methods.setOwner(userWallet).encodeABI();
-      let txParams = {
-        nonce,
-        chainId,
-        to: ticketInstance.options.address,
-        value: 0,
-        gas,
-        gasPrice,
-        data: encodedAbi
-      };
+        let encodedAbi = ticketInstance.methods.masterBuy(userWallet).encodeABI();
+        let txParams = {
+          nonce,
+          chainId,
+          to: ticketInstance.options.address,
+          value: 0,
+          gas,
+          gasPrice,
+          data: encodedAbi
+        };
 
-      let tx = new EthereumTx(txParams);
-      let privateKeyBuffer = new Buffer(Buffer.from(wallet.privateKey.substring(2), 'hex'));
-      tx.sign(privateKeyBuffer);
-      let serializedTx = tx.serialize();
+        let tx = new EthereumTx(txParams);
+        let privateKeyBuffer = new Buffer(Buffer.from(wallet.privateKey.substring(2), 'hex'));
+        tx.sign(privateKeyBuffer);
+        let serializedTx = tx.serialize();
 
-      console.log('sending signed tx');
-      // console.log(await ticketInstance.methods.master().call());
-      // console.log(wallet.address);
-      await web3.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`);
+        await web3.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`);
 
-      let newOwner = await ticketInstance.methods.owner().call();
-      console.log('newOwner:', newOwner);
-      // return
+        let newOwner = await ticketInstance.methods.owner().call();
+        console.log('newOwner:', newOwner);
+        // return
+      });
       res.send('success');
     } catch (e) {
       console.log('Setting Owner failed', e);
       res.send('failed');
     }
-
     next();
   });
 };
